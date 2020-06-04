0955869e5417c11ecafe53689b4381b296cb175f
Diego Braun <diegobraun2000@gmail.com>
2020-06-03 22:04:49 -0300

    Adding a class to push more easily the content of a json file, and refactoring api tests

--- algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -1,34 +1,57 @@
 package com.algaworks.algafood;
 
-import static org.hamcrest.Matchers.*;
+import static com.algaworks.algafood.util.ResourceUtils.getContentFromResource;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+
+import java.io.IOException;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static io.restassured.RestAssured.given;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.util.DatabaseCleaner;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 public class CadastroCozinhaIT {
+	
+	private static final int COZINHA_ID_INEXISTENTE = 10;
+	
+	private int quantidadeCozinhasNoRepositorio;
 	
 	@LocalServerPort
 	private int port;
+	
+	@Autowired
+	private DatabaseCleaner databaseCleaner;
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
 	
 	@Before
 	public void setUp() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
+		
+		databaseCleaner.clearTables();
+		prepararDados();
+		quantidadeCozinhasNoRepositorio = cozinhaRepository.findAll().size();
 	}
 	
 	@Test
@@ -42,13 +65,63 @@ public class CadastroCozinhaIT {
 	}
 	
 	@Test
-	public void deveConter3CozinhasQuandoConsultarCozinhas(){
+	public void deveConter2CozinhasQuandoConsultarCozinhas(){
 		given()
 			.accept(ContentType.JSON)
 		.when()
 			.get()
 		.then()
-			.body("", hasSize(3))
-			.body("nome", hasItems("tailandesa","indiana"));
+			.body("", hasSize(quantidadeCozinhasNoRepositorio));
+	}
+	
+	@Test
+	public void deveRetornarStatus201QuandoCadastrarCozinha() throws IOException {
+		given()
+			.body(getSingleCozinha())
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(HttpStatus.CREATED.value());
+	}
+	
+	//GET /cozinhas/{cozinhaId}
+	
+	@Test
+	public void deveRetornarRespostaEStatusCorretosQuandoConsultarCozinhaExistente() {
+		given()
+			.pathParam("cozinhaId", 2)
+			.accept(ContentType.JSON)
+		.when()
+			.get("/{cozinhaId}")
+		.then()
+			.statusCode(HttpStatus.OK.value())
+			.body("nome", equalTo("americana"));
+	}
+	
+	@Test
+	public void deveRetornarStatus404QuandoConsultarCozinhaInexistente() {
+		given()
+			.pathParam("cozinhaId", COZINHA_ID_INEXISTENTE)
+			.accept(ContentType.JSON)
+		.when()
+			.get("/{cozinhaId}")
+		.then()
+			.statusCode(HttpStatus.NOT_FOUND.value());
+	}
+	
+	private void prepararDados() {
+		Cozinha cozinha1 = new Cozinha();
+		cozinha1.setNome("tailandesa");
+		cozinhaRepository.save(cozinha1);
+		
+		Cozinha cozinha2 = new Cozinha();
+		cozinha2.setNome("americana");
+		cozinhaRepository.save(cozinha2);
+	}
+	
+	private String getSingleCozinha() throws IOException {
+		return getContentFromResource("/json/cozinhaSingle.json");
 	}
 }
